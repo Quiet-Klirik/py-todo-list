4501476add2692c4d7a0b5f524089883b15dae76
Yaroslav Lysiuk <quiet.klirik@gmail.com>
2024-02-19 14:23:12 +0200

    Tag CRUD
     - add [tag_list.html, tag_form.html, tag_confirm_delete.html] templates
     - add [TagListView, TagCreateView, TagUpdateView, TagDeleteView] views and connect they to the [todo_list.urls]
     - several fixes in models and templates

--- todo_list/views.py
+++ todo_list/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 
-from todo_list.models import Task
+from todo_list.models import Task, Tag
 
 
 class TaskListView(generic.ListView):
@@ -13,18 +13,18 @@ class TaskListView(generic.ListView):
 class TaskCreateView(generic.CreateView):
     model = Task
     fields = ["content", "deadline", "tags"]
-    success_url = reverse_lazy("todo:list")
+    success_url = reverse_lazy("todo:task_list")
 
 
 class TaskUpdateView(generic.UpdateView):
     model = Task
     fields = ["content", "deadline", "tags"]
-    success_url = reverse_lazy("todo:list")
+    success_url = reverse_lazy("todo:task_list")
 
 
 class TaskDeleteView(generic.DeleteView):
     model = Task
-    success_url = reverse_lazy("todo:list")
+    success_url = reverse_lazy("todo:task_list")
 
 
 class TaskSwitchView(generic.View):
@@ -44,4 +44,25 @@ class TaskSwitchView(generic.View):
         else:
             object.is_done = True
         object.save()
-        return redirect("todo:list")
+        return redirect("todo:task_list")
+
+
+class TagListView(generic.ListView):
+    model = Tag
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = ["name"]
+    success_url = reverse_lazy("todo:tag_list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = ["name"]
+    success_url = reverse_lazy("todo:tag_list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("todo:tag_list")

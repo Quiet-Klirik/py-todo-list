2609a9fc774379657e90484a71f9b8675a57eddc
Yaroslav Lysiuk <quiet.klirik@gmail.com>
2024-02-19 15:52:41 +0200

    Add [/] to end of the urls

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -14,18 +14,30 @@ from todo_list.views import (
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task_list"),
-    path("tasks/create", TaskCreateView.as_view(), name="task_create"),
-    path("tasks/<int:pk>/update", TaskUpdateView.as_view(), name="task_update"),
-    path("tasks/<int:pk>/delete", TaskDeleteView.as_view(), name="task_delete"),
     path(
-        "task/<int:pk>/switch-status",
+        "tasks/create/",
+        TaskCreateView.as_view(),
+        name="task_create"
+    ),
+    path(
+        "tasks/<int:pk>/update/",
+        TaskUpdateView.as_view(),
+        name="task_update"
+    ),
+    path(
+        "tasks/<int:pk>/delete/",
+        TaskDeleteView.as_view(),
+        name="task_delete"
+    ),
+    path(
+        "task/<int:pk>/switch-status/",
         TaskSwitchView.as_view(),
         name="task-status-switch"
     ),
     path("tags/", TagListView.as_view(), name="tag_list"),
-    path("tags/create", TagCreateView.as_view(), name="tag_create"),
-    path("tags/<int:pk>/update", TagUpdateView.as_view(), name="tag_update"),
-    path("tags/<int:pk>/delete", TagDeleteView.as_view(), name="tag_delete"),
+    path("tags/create/", TagCreateView.as_view(), name="tag_create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag_update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag_delete"),
 ]
 
 app_name = "todo_list"

271717e01be9392941b3b7cac6296147441d662e
Yaroslav Lysiuk <quiet.klirik@gmail.com>
2024-02-19 14:35:19 +0200

    settings.DEBUG = False

--- core/settings.py
+++ core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$asm#wayrlg_7%mt^phnbqo@%vzpdg8=1#7k9bx!p@5+7)q@8j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
